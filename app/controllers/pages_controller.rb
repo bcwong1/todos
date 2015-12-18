5489d3cb18b84d4a2666367b675ced321ad3e483
bcwong1 <bcwong1@ualberta.ca>
2015-12-17 18:46:07 -0700

    added link detection

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,19 @@
 class PagesController < ApplicationController
-  def home
+
+def home
+  if current_user
+    redirect_to tasks_path
   end
+end
+
 
   def about
   end
   
+  def test
+
+  end
+
+
+
 end
